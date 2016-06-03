64e4c22b835a13781becdca5f299430815731c50
DreamFly111 <1320027594@qq.com>
2016-06-03 12:13:48 +0800

    Initial commit

---
+++ app/src/main/java/duoy/cn/wifitest/MainActivity.java
@@ -0,0 +1,44 @@
+package duoy.cn.wifitest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import junit.framework.Test;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+
+        isWifiConnected(this);
+    }
+
+    public void isWifiConnected(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if(networkInfo.isConnected()){
+            Log.i("test","true");
+            Intent intent = new Intent(context, Test1.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Intent intent = new Intent(context,Test2.class);
+            startActivity(intent);
+            Log.i("test","false");
+            finish();
+        }
+    }
+}
